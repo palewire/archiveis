496ecb012ac043d76a62ade1c3dd89b57d4015d5
palewire <ben.welsh@gmail.com>
2018-08-07 09:56:40 -0700

    v0.0.6

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='archiveis',
-    version='0.0.5',
+    version='0.0.6',
     description='A simple Python wrapper for the archive.is capturing service.',
     author='Ben Welsh',
     author_email='ben.welsh@gmail.com',
